a55c385b5dff51b97c6093ec7088c32b07d7c664
Josh Ford <thisisjoshford@gmail.com>
2025-03-05 10:59:11 -0800

    test: upload same version

--- agent/agent.py
+++ agent/agent.py
@@ -3,6 +3,7 @@ from nearai.agents.environment import Environment
 
 def run(env: Environment):
     # Your agent code here
+    # TEST
     prompt = {"role": "system", "content": "gh-upload-test"}
     result = env.completion([prompt] + env.list_messages())
     env.add_reply(result)
