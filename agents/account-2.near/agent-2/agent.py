320d8b5d25ad1552dfcefd2bae3109d6179a2fb9
Josh Ford <thisisjoshford@gmail.com>
2025-03-10 09:39:42 -0700

    feat: add multi-agents / accounts

---
+++ agents/account-2.near/agent-2/agent.py
@@ -0,0 +1,13 @@
+from nearai.agents.environment import Environment
+
+
+def run(env: Environment):
+    # Your agent code here
+    # Agent 2
+    prompt = {"role": "system", "content": "gh-upload-test"}
+    result = env.completion([prompt] + env.list_messages())
+    env.add_reply(result)
+    env.request_user_input()
+
+run(env)
+
